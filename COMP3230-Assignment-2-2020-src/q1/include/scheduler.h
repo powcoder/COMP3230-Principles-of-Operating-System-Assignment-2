6ee0c38fb52f5adc48c65c2019b175d7980d111c
powcoder <powcoder@163.com>
2022-11-25 15:26:07 +0800

    COMP3230 Principles of Operating System Assignment 2

---
+++ COMP3230-Assignment-2-2020-src/q1/include/scheduler.h
@@ -0,0 +1,22 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#ifndef SCHEDULER_H_3PFXQEN0
+#define SCHEDULER_H_3PFXQEN0
+
+#include "../include/production.h"
+#include "../include/robot.h"
+#include "../include/utils.h"
+
+// simpleWork() and simpleRobotRoutine() are simple examples, 
+// you can create your own scheduling functions
+void simpleWork(int jobID, Robot robot); 
+void* simpleRobotRoutine(void* arg);
+
+
+#endif /* end of include guard: SCHEDULER_H_3PFXQEN0 */
